559515b58b7e3a68aa389ce89160dd29bc096c3a
Andrew White <drew.white@aa.com>
2017-05-13 17:45:36 -0500

    FormGroup complete

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
-  name = new FormControl();
+export class HeroDetailComponent {
+  heroForm = new FormGroup({
+    name: new FormControl()
+  });
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
